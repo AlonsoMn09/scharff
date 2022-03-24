916d2242545769bfeda0aaf93c4a9e9b93a506a6
Manuel C. G <manu_cg19@hotmail.com>
2022-03-24 15:23:00 +0100

    #Error

--- Sharff.ApiRest/Controllers/ExampleController.cs
+++ Sharff.ApiRest/Controllers/ExampleController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Sharff.ApiRest.Models;
 using Sharff.Core.Services.Interfaces;
 using System.Threading.Tasks;
@@ -10,12 +11,15 @@ namespace Sharff.ApiRest.Controllers
     [ApiController]
     public class ExampleController : ControllerBase
     {
+        private readonly ILogger<ExampleController> _logger;
+
         public readonly IMapper _mapper;
 
         public readonly IExampleService _exampleService;
 
-        public ExampleController(IMapper mapper, IExampleService exampleService)
+        public ExampleController(ILogger<ExampleController> logger, IMapper mapper, IExampleService exampleService)
         {
+            this._logger = logger;
             this._mapper = mapper;
             this._exampleService = exampleService;
         }
@@ -23,14 +27,22 @@ namespace Sharff.ApiRest.Controllers
         [HttpGet()]
         public async Task<ActionResult<ExampleDto>> GetExample()
         {
-            var resultService = await this._exampleService.GetExampleAsync();
-
             var result = new ResultDto
             {
                 StatusCode = 200
             };
-            result.Payload = this._mapper.Map<ExampleDto>(resultService);
-            return Ok(result);
+
+            try
+            {
+                var resultService = await this._exampleService.GetExampleAsync();
+                result.Payload = this._mapper.Map<ExampleDto>(resultService);
+                return Ok(result);
+            }
+            catch (System.Exception ex)
+            {
+                this._logger.LogError(ex.Message, ex.InnerException);
+            }
+            return BadRequest(result);
         }
     }
 }
